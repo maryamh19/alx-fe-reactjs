221a7c9f7cc1d6e37b4d1339c65fce8f0d2a22f5
maryam hatem <maryamhatem19@gmail.com>
2026-01-12 17:20:40 +0200

    fixx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className="flex justify-between items-center p-2.5 bg-[#077d92e0] text-white">
-      <Link to="/" className="mx-2.5 text-white">Home</Link>
-      <Link to="/about" className="mx-2.5 text-white">About</Link>
-      <Link to="/services" className="mx-2.5 text-white">Services</Link>
-      <Link to="/contact" className="mx-2.5 text-white">Contact</Link>
+      <div className="flex gap-5">
+        <Link to="/" className="text-white">Home</Link>
+        <Link to="/about" className="text-white">About</Link>
+        <Link to="/services" className="text-white">Services</Link>
+        <Link to="/contact" className="text-white">Contact</Link>
+      </div>
     </nav>
   );
 }
