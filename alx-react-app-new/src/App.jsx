c48a39300c35a0b3b53d7cecee47ef1f68731234
maryam hatem <maryamhatem19@gmail.com>
2026-01-12 15:28:44 +0200

    Added counter buttons

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
-
+import Counter from './components/Counter.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,8 +18,11 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
-      <Footer />
+      <Counter />
 
+
+
+ <Footer />
     </>
   )
 }
