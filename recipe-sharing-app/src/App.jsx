44479e24ccede139fd126dac484e5bd3663b32be
maryam hatem <maryamhatem19@gmail.com>
2026-01-22 17:45:28 +0200

    initial commit: Recipe sharing app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+};
+
+export default App;
