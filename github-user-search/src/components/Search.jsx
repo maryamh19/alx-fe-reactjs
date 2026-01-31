cdfd6d3eea28ab2c24e87a827009d39a340e776f
maryam hatem <maryamhatem19@gmail.com>
2026-01-31 22:46:34 +0200

    changed githubservice name

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { fetchUserData } from "../services/githubApi";
+import { fetchUserData } from "../services/githubService";
 
 export default function Search() {
   const [username, setUsername] = useState("");
