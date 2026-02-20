3eab307999b0648db1052817e2f079c6ba691a7d
maryam hatem <maryamhatem19@gmail.com>
2026-02-21 00:33:38 +0200

    babel and jest fixattions

--- react-todo/src/setupTests.js
+++ react-todo/src/setupTests.js
@@ -1,1 +1,27 @@
 import "@testing-library/jest-dom";
+import { JSDOM } from "jsdom";
+
+// Initialize JSDOM once
+const dom = new JSDOM("<!DOCTYPE html><html><body><div id='root'></div></body></html>", {
+  url: "http://localhost",
+});
+
+// Set global variables
+Object.defineProperty(global, "document", {
+  value: dom.window.document,
+  writable: false,
+});
+
+Object.defineProperty(global, "window", {
+  value: dom.window,
+  writable: false,
+});
+
+Object.defineProperty(global, "navigator", {
+  value: dom.window.navigator,
+  writable: false,
+});
+
+global.HTMLElement = dom.window.HTMLElement;
+global.Element = dom.window.Element;
+global.Node = dom.window.Node;
